e7b003d42877094753c15eeac466ce73db16a375
Jan Michael Tangonan <janmichael.tangonan.2022@student.lhschools.org>
2019-02-07 12:53:59 -0500

    Initialized variables

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -8,6 +8,16 @@ public class Main {
         String secondWord;
         int firstNumber, secondNumber, total;
 
+        firstWord = "dog";
+        secondWord = "house";
+
+        firstNumber = 5;
+        secondNumber = 10;
+        total = firstNumber + secondNumber;
+        System.out.println(total);
+        total = total + 10;
+        System.out.println(total);
+
 
 
     }
